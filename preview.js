fcf5bb696256ae7dca499bacd2cff14b30bbb5b7
AsteroidLord <84286534+asteroidlordfr@users.noreply.github.com>
2026-01-09 13:02:33 -0500

    Update preview.js

--- preview.js
+++ preview.js
@@ -5,9 +5,17 @@ function renderPreview(objects) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   let x = 10;
+
   objects.forEach(obj => {
-    ctx.fillStyle = "#0ff";
-    ctx.fillRect(x, 100, 20, 20);
-    x += 15;
+    const id = parseInt(obj.split(",")[1]);
+
+    ctx.fillStyle =
+      id === 8 ? "#f33" :      // spike
+      id === 36 ? "#ff0" :     // orb
+      id >= 600 ? "#0ff" :     // wave portal
+      "#0f0";                  // block
+
+    ctx.fillRect(x, 110, 18, 18);
+    x += 8;
   });
 }
